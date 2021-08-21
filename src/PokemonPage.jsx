876c4956f0fbfeb656a4e3ccd5365765df606a80
hubofthewest <hubofthewest@gmail.comm>
2021-08-21 14:26:33 +1000

    end 11-8

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -32,6 +32,7 @@ const PokemonPage = ({ previous, next }) => {
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
+        {/* previous to next */}
         {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
