8b8a100b96b9f340f2dc0a7ed211be442cdfe27b
hubofthewest <hubofthewest@gmail.comm>
2021-08-22 15:22:36 +1000

    end 11-9

--- cypress/integration/EndToEnd.jsx
+++ cypress/integration/EndToEnd.jsx
@@ -1,7 +1,7 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
     // eslint-disable-next-line no-undef
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:5000')
     // eslint-disable-next-line no-undef
     cy.contains('ivysaur')
     // eslint-disable-next-line no-undef
