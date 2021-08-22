b1b82e6e09162933e97853e886812b32f239a047
hubofthewest <hubofthewest@gmail.comm>
2021-08-22 16:23:37 +1000

    real end 11-9

--- cypress/integration/EndToEnd.jsx
+++ cypress/integration/EndToEnd.jsx
@@ -7,4 +7,12 @@ describe('Pokedex', function () {
     // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('navigate Caterpie page', function () {
+    // eslint-disable-next-line no-undef
+    cy.visit('http://localhost:5000')
+    // eslint-disable-next-line no-undef
+    cy.contains('caterpie').click()
+    // eslint-disable-next-line no-undef
+    cy.contains('run away')
+  })
 })
